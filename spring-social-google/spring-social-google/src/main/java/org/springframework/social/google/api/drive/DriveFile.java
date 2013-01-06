1223069d5ee222d2a418ab29697af3d4f6d1cfb3
Christian Schröder <christian.schroeder@conceptboard.com>
2013-01-06 17:35:35 +0100

    Revert "null-safety for setting parents"
    This reverts commit 51a96f8b6e24eb48d458b7d42bb1321bfd795070.

--- spring-social-google/spring-social-google/src/main/java/org/springframework/social/google/api/drive/DriveFile.java
+++ spring-social-google/spring-social-google/src/main/java/org/springframework/social/google/api/drive/DriveFile.java
@@ -27,8 +27,6 @@ import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.springframework.social.google.api.ApiEntity;
 
-import com.google.common.base.Strings;
-
 /**
  * Model class representing a file in Google Drive
  * @author Gabriel Axel
@@ -140,9 +138,7 @@ public class DriveFile extends ApiEntity {
 			file.parents = new ArrayList<DriveFileParent>();
 			file.thumbnail = thumbnail;
 			for(String parentId : parentIds) {
-				if(parentId != null){
-					file.parents.add(new DriveFileParent(parentId));
-				}
+				file.parents.add(new DriveFileParent(parentId));
 			}
 			return file;
 		}
